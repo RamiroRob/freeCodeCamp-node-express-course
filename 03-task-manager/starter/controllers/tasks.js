09a6ea4c5ef0c4d666ddc4e54ba27df0ef92ed07
Ramiro Roballos <ramiroroballos@gmail.com>
2023-03-05 09:05:57 -0500

    getOneTask and delete task

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,29 +1,57 @@
 const taskSchema = require("../models/task")
 
-
-function getAllTasks (req,res) {
-    res.send("All items Controller")
-}
-
-function getTask (req,res) {
-    res.json({id:req.params.id})
-}
-
-async function createTask (req,res) {
+async function getAllTasks(req, res) {
     try {
-        const Task = await taskSchema.create(req.body)
-         res.status(201).json({Task})
+        const getTasks = await taskSchema.find({})
+        res.status(200).json({ getTasks })
     } catch (error) {
-        res.status(500).json({message: error})
+        res.status(500).json({ message: error })
     }
 }
 
-function updateTask (req,res) {
+async function getTask(req, res) {
+
+    try {
+        const { id: taskID } = req.params
+        const task = await taskSchema.findOne({ _id: taskID })
+
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` })
+        }
+
+        res.status(200).json({ task })
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+async function createTask(req, res) {
+    try {
+        const createTask = await taskSchema.create(req.body)
+        res.status(201).json({ createTask })
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+}
+
+function updateTask(req, res) {
     res.send("update task")
 }
 
-function deleteTask (req,res) {
-    res.send("delete task")
+async function deleteTask(req, res) {
+    try {
+       const { id: taskID } = req.params 
+       const task = await taskSchema.findOneAndDelete({_id: taskID})
+
+       if(!task) return res.status(404).json({msg: `task with id ${taskID} doesn't exist`})
+
+       res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({ message: error })
+
+    }
+    
 }
 
 
