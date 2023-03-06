79127b2a17f543f629b40741841dcb346d997710
Ramiro Roballos <ramiroroballos@gmail.com>
2023-03-05 19:44:31 -0500

    comienzo del proyecto store API

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+const dotenv = require("dotenv")
+dotenv.config()
+const express = require("express")
+const connectDB = require("./db/connect")
+const productsRouter = require("./routes/products")
+
+
+const app = express()
+
+const notFoundMIddleware = require("./middleware/not-found")
+const errorMiddleware = require("./middleware/error-handler")
+
+
+app.use(express.json())
+
+app.get("/", (req,res)=> {
+    res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>")
+})
+
+app.use("/api/v1/products", productsRouter)
+
+
+app.use(notFoundMIddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
